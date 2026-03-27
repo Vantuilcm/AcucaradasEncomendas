8493285ceac2918a6cf60316db64edc88c20eb1f
Vantuilcm <vantuilcaramez@gmail.com>
2026-03-27 16:32:49 -0300

    fix(android): force exclusion of legacy support libraries and refine manifest fix

--- plugins/withAndroidNamespaceFix.js
+++ plugins/withAndroidNamespaceFix.js
@@ -32,7 +32,10 @@ allprojects {
             force 'androidx.appcompat:appcompat:1.6.1'
             force 'androidx.vectordrawable:vectordrawable:1.1.0'
             force 'androidx.vectordrawable:vectordrawable-animated:1.1.0'
+            force 'androidx.fragment:fragment:1.3.6'
         }
+        // Excluir bibliotecas legadas que causam conflito de namespace
+        exclude group: 'com.android.support'
     }
 }
 `;
@@ -54,12 +57,14 @@ function fixNamespaces(contents) {
 android.applicationVariants.all { variant ->
     variant.outputs.all { output ->
         output.processManifestProvider.get().doLast { manifest ->
-            def manifestFile = manifest.outputs.files.first()
-            if (manifestFile.exists()) {
-                def content = manifestFile.text
-                if (!content.contains('tools:replace="android:appComponentFactory"')) {
-                    content = content.replace('<application', '<application tools:replace="android:appComponentFactory" xmlns:tools="http://schemas.android.com/tools"')
-                    manifestFile.text = content
+            def manifestFiles = manifest.outputs.files.files
+            manifestFiles.each { manifestFile ->
+                if (manifestFile.exists() && manifestFile.name == "AndroidManifest.xml") {
+                    def content = manifestFile.text
+                    if (content.contains('<application') && !content.contains('tools:replace="android:appComponentFactory"')) {
+                        content = content.replace('<application', '<application tools:replace="android:appComponentFactory" xmlns:tools="http://schemas.android.com/tools"')
+                        manifestFile.text = content
+                    }
                 }
             }
         }
